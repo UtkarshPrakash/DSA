495b302d276005423111eb0bc413394b712d1309
UtkarshPrakash <utkarshprakash21sep@gmail.com>
2022-03-29 22:26:26 +0530

    changes to Contains Duplicate.cpp

--- Contains Duplicate.cpp
+++ Contains Duplicate.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int> check;
         int n = nums.size();
         for (int i=0; i<n; i++){
-            if (check[nums[i]]){
+            if (check[nums[i]]){ // using if(m.find(nums[i])!=m.end()) reduced time
                 return true;
             }
             check[nums[i]] = 1;
