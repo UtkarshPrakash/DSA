f02785e30b91b18750f5d46d227a7c1c9f2ab22e
UtkarshPrakash <48208181+UtkarshPrakash@users.noreply.github.com>
2022-04-27 15:14:17 +0530

    leetcode

--- To Lower Case.cpp
+++ To Lower Case.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-    string toLowerCase(string s) { 
-        for (int i=0; i<s.length(); i++){
-            if (s[i]>64 && s[i]<91)
-                s[i] += 32;
-        }
-        return s; // The fastest method would be to use transform()
+    string toLowerCase(string s) {
+        // Method 1
+        for (int i=0; i<s.length(); i++)
+            s[i] = tolower(s[i]);
+        return s;
+
+
+        // Method 2 
+        // for (int i=0; i<s.length(); i++){
+        //     if (s[i]>64 && s[i]<91)
+        //         s[i] += 32;
+        // }
+        // return s;
+
+
+        // Method 3
+        // use std::transform 
     }
 };
