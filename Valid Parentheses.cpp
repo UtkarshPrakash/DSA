75e20f9a03d039b3818bfebb9d6e51c45750e9e5
UtkarshPrakash <48208181+UtkarshPrakash@users.noreply.github.com>
2022-04-28 17:05:59 +0530

    leetcode

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -1,11 +1,31 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<string> stack;
-        string op[] = {'(', '{', '['};
-        string cl[] = {')', '}', '['};
-        if (find(cl, cl+3, s[0]))
+        stack<char> stack;
+        char op[] = {'(', '{', '['};
+        char cl[] = {')', '}', ']'};
+        map<char, char> open;
+        open.insert(pair<char,char>('(', ')'));
+        open.insert(pair<char,char>('{','}'));
+        open.insert(pair<char,char>('[',']'));
+        if (s[0]==')' || s[0]=='}' || s[0]==']')
             return false;
         stack.push(s[0]);
+        for (int i=1; i<s.length(); i++){
+            if (s[i]==')' || s[i]=='}' || s[i]==']'){
+                if (!stack.size())
+                    return false;
+                if (s[i]==open[stack.top()])
+                    stack.pop();
+                else
+                    return false;
+            }
+            else
+                stack.push(s[i]);
+        }
+        if (stack.size())
+            return false;
+        else
+            return true;
     }
 };
