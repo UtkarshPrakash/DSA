9a44bfd2f958dbca8fa3f69850c12162f360713d
UtkarshPrakash <48208181+UtkarshPrakash@users.noreply.github.com>
2022-04-28 16:45:50 +0530

    leetcode

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<string> stack;
+        string op[] = {'(', '{', '['};
+        string cl[] = {')', '}', '['};
+        if (find(cl, cl+3, s[0]))
+            return false;
+        stack.push(s[0]);
     }
 };
